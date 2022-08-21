f5bd33507c6c4614464cdd13139a1ca915245e08
Fabiano Landim <landimfabiano01@gmail.com>
2022-08-21 15:17:29 -0300

    Update index.py

--- main_file/index.py
+++ main_file/index.py
@@ -14,7 +14,8 @@ import PySimpleGUI as sg
 # Manutenção: Fabiano Landim <landimfabiano01@gmail.com>
 #
 # ESCOPO:
-# O Programa nada mais é que uma interface gráfica desnvolvida em PySimpleGUI, com o intuito de proporcionar um jogo de dados simulando uma disputa entre o computador e você,onde ambos escolhem um número de 1 à 6, no qual resultará em um ganhador, empate, ou nenhum ganhador.
+# O Programa nada mais é que uma interface gráfica desnvolvida em PySimpleGUI, com o intuito de proporcionar um jogo de dados
+# simulando uma disputa entre o computador e você,onde ambos escolhem um número de 1 à 6, no qual resultará em um ganhador, empate, ou nenhum ganhador.
 #
 # Histórico:
 #
