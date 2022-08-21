17ea45075df43945a7017b5d92d46b2d7441d89a
Fabiano Landim <landimfabiano01@gmail.com>
2022-08-21 11:56:45 -0300

    Create index.py
    Criando o cabeçalho do programa.

---
+++ main_file/index.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import random
+import PySimpleGUI
+
+#Nome_do _programa: Jogo Dado
+#
+#site: 
+
+#ESCOPO: O Programa nada mais é que uma interface gráfica desnvolvida em PySimpleGUI, com o intuito de proporcionar um jogo de dados simulando uma disputa entre o computador e você,onde ambos escolhem um número de 1 à 6, no qual resultará em um ganhador, empate, ou nenhum ganhador.
+
+#Autor: Fabiano Landim <landimfabiano01@gmail.com>
+#Manutenção: Fabiano Landim <landimfabiano01@gmail.com>
